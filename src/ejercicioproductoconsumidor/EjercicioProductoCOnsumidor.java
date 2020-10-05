001908ef03eb45bc497959e3f9c9b1b79a9ef69a
Andrés Sánchez <anfurtivo1@hotmail.com>
2020-10-05 16:26:13 +0200

    Primer commit

---
+++ src/ejercicioproductoconsumidor/EjercicioProductoCOnsumidor.java
@@ -0,0 +1,19 @@
+
+package ejercicioproductoconsumidor;
+
+/**
+ *
+ * @author Andrés
+ */
+public class EjercicioProductoCOnsumidor {
+
+    /**
+     *El productor generara un producto y el consumidor luego lo consumirá. Compartiran un objeto compartido y el productor no generará otro
+     *producto hasta que el consumidor lo quiera.Ejercicio8
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
